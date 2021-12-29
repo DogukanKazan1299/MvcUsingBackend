b8cd7958950844f5e7d75080eb2e07ccb863b2df
DoğukanKazan <kazand25@gmail.com>
2021-12-29 13:04:13 +0300

    MvcUsingBackend
    My asp.net core mvc project using asp.net core web api

---
+++ MvcUsingBackend/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ MvcUsingBackend/WebServer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebServer.Helper;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductAPI _api = new ProductAPI();
+        public async Task<IActionResult> Index()
+        {
+            List<ProductData> products = new List<ProductData>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/products/get");
+            if (res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<List<ProductData>>(results);
+            }
+            return View(products);
+        }
+    }
+}
