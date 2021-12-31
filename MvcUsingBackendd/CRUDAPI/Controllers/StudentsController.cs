06e5dd1bf516d8a4b4a08b7f114f65e3183cc7c8
DoğukanKazan <kazand25@gmail.com>
2021-12-31 14:18:58 +0300

    MvcUsingBackend
    asp.net core mvc using backend

---
+++ MvcUsingBackendd/CRUDAPI/Controllers/StudentsController.cs
@@ -0,0 +1,58 @@
+﻿using CRUDAPI.Data;
+using CRUDAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationProject.Models;
+
+namespace CRUDAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        Context _context;
+        public StudentsController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return _context.Students.ToList();
+        }
+        [HttpGet("{Id}")]
+        public Student GetStudent(int Id)
+        {
+            var student = _context.Students.Where(a => a.Id == Id).SingleOrDefault();
+            return student;
+        }
+        [HttpPost]
+        public IActionResult PostStudent([FromBody]Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Not a valid model");
+            }
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return Ok();
+            
+        }
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
